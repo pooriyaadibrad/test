f16306c1398394015a12708c4bcdc94d913cb4a0
pooriya adib <adibradp@gmail.com>
2024-02-22 15:32:24 +0330

    this is final step in git

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from tkinter import *
 win=Tk()
 
 win.geometry("800x600")
-
+def test(e):
+    print("hi")
 btn=Button(win,text="Click ")
-btn.bind()
+btn.bind("<Button-1>",test)
 btn.pack(side="top",fill=BOTH)
 
 win.mainloop()
